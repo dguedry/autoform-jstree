24743f874af24d9b78452cc4e7595aad61c67575
Donnie Guedry <dguedry@gmail.com>
2017-04-26 01:52:20 -0500

    Update package name

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
-  name: 'artjomg:autoform-jstree',
-  version: '0.0.1',
+  name: 'dguedry:autoform-jstree',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'jsTree input for Autoform',
   // URL to the Git repository containing the source code for this package.
